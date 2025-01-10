8084d4702cc54f99a4848d6ff4a18cdbb4bb3915
suriya216 <136613620+suriya216@users.noreply.github.com>
2025-01-10 19:07:13 +0530

    equilateralTriangle

---
+++ equilateralTriangle.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int N=sc.nextInt();
+		
+		for(int i=0;i<=N;i++){
+		    for(int j=1;j<=N-i;j++){
+		        System.out.print(" ");
+		    }
+		    for(int j=1;j<=i;j++){
+		        System.out.print(i+" ");
+		    }
+		    System.out.println();
+		}
+		sc.close();
+	}
+}
